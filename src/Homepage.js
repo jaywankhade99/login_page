10fddf9ad3b0308cd9b745d134a0043fde27c9ff
jaywankhade99 <jaywankhade99@gmail.com>
2022-03-21 18:29:32 +0530

    registration page

---
+++ src/Homepage.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Homepage extends Component{
+    render(){
+        return(
+            <>
+            <div className="App-header">
+                <h1>Welcome to Atos</h1>
+                <input classname= 'username'
+                type="text"
+                placeholder=" Enter Username"/>
+
+
+
+
+
+            </div>
+            </>
+        )
+    }
+    
+}
+export default Homepage;
